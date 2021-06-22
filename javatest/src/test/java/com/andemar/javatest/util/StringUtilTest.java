6e5611fd4906b44934e68cf01bf2fc79fcbaa702
Anderson <andemartinezdev@gmail.com>
2021-06-21 22:18:20 -0500

    3-4 -> Uso de Junit 5
    - Se implemento un metodo con el decorador test para la ejecucion de pruebas.

--- javatest/src/test/java/com/andemar/javatest/util/StringUtilTest.java
+++ javatest/src/test/java/com/andemar/javatest/util/StringUtilTest.java
@@ -1,12 +1,15 @@
 package com.andemar.javatest.util;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class StringUtilTest {
 
-    public static void main(String[] args) {
-
+    @Test
+    public void testRepeat() {
         String result = StringUtil.repeat("hola", 3);
-
-        System.out.println(result);
+        Assertions.assertEquals("holaholahola", result);
     }
 
 }

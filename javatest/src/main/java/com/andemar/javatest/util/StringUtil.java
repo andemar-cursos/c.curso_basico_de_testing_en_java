ca091cd223e429319d202c393f9f08218183d26a
Anderson <andemartinezdev@gmail.com>
2021-06-21 22:03:58 -0500

    3-2 -> Init project
    - Se creo el proyecto.

---
+++ javatest/src/main/java/com/andemar/javatest/util/StringUtil.java
@@ -0,0 +1,17 @@
+package com.andemar.javatest.util;
+
+public class StringUtil {
+
+
+    public static String repeat(String str, int times) {
+
+        String result = "";
+
+        for (int i = 0; i < times; i++) {
+            result += str;
+        }
+
+        return result;
+    }
+
+}
---
+++ javatest/src/test/java/com/andemar/javatest/util/StringUtilTest.java
@@ -0,0 +1,12 @@
+package com.andemar.javatest.util;
+
+class StringUtilTest {
+
+    public static void main(String[] args) {
+
+        String result = StringUtil.repeat("hola", 3);
+
+        System.out.println(result);
+    }
+
+}
